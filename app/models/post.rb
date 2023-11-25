fedba636994b90f74c2969ee92667012bdc5ac37
takokke <138221752+takokke@users.noreply.github.com>
2023-11-25 01:45:20 +0000

    comment機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  
+  has_many :post_comments, dependent: :destroy
 
   validates :creature_name, presence: true
   validates :image, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get '/about' => "homes#about"
-  resources :posts
+  resources :posts do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resource :map, only: [:show]
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
