42e56640006ff2176d56ed624f7c4d1e341cc50a
takokke <138221752+takokke@users.noreply.github.com>
2023-12-17 00:58:56 +0000

    map修正

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,4 +1,5 @@
 // ブートストラップ ローダ
+// Dynamic Library Import を使用して導入
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
   key: process.env.API_KEY
 });
@@ -85,7 +86,7 @@ async function initMap() {
             </div>
             <div>
               <h1 class="h4 font-weight-bold">${item.creature_name}</h1>
-              <p class="text-muted">${item.address}</p>
+              <p class="text-muted" style="width: 220px;">${item.address}</p>
               <p class="lead" style="width: 220px;">${item.caption}</p>
             </div>
           </div>
