7e79b95ced9896d0515d87ffcb9ebed23d3ed6c1
takumi <takumicr1526@gmail.com>
2024-02-02 01:29:12 +0000

    ログイン画面レイアウト途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  
+  # フラッシュメッセージ用
   add_flash_types :secondary, :success, :danger, :warning, :info, :light, :dark
 
+  # ログイン後のページ遷移
   def after_sign_in_path_for(resource)
     posts_path
   end
 
+  # ログアウト後のページ遷移
   def after_sign_out_path_for(resource)
     about_path
   end
 
   protected
-
+   
+  # deviseのストロングパラメータ
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
