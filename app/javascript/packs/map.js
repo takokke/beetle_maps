ec93b3e6cd4fdc3b6999343723a0fdc3235f3e14
takumi <takumicr1526@gmail.com>
2024-02-02 17:36:43 +0000

    新規登録画面 map.js修正

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -8,10 +8,11 @@
 let map;
 
 async function initMap() {
+  console.log("マップ生成開始")
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 34.6648863, lng: 133.916252 }, 
+    center: { lat: 34.6648863, lng: 133.916252 },
     zoom: 15,
     mapTypeControl: true
   });
@@ -19,14 +20,15 @@ async function initMap() {
   let geocoder = new google.maps.Geocoder();
   // マーカーの初期化
   let marker;
-  
+
   // 地図の検索
-  $('#search').on('click', function() {
+
+
+  $("#search").on('click', function() {
     let place = $("#keyword").val();
-    // let geocoder = new google.maps.Geocoder();
     geocoder.geocode({
       address: place
-    }, function(results, status) {
+    }, (results, status) => {
       if (status == google.maps.GeocoderStatus.OK) {
         let bounds = new google.maps.LatLngBounds();
         for (let i in results) {
@@ -39,32 +41,33 @@ async function initMap() {
           }
         }
       } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
-        alert("見つかりません");
+        console.log(status);
+        // alert("見つかりません");
       } else {
         console.log(status);
-        alert("エラー発生");
+        // alert("エラー発生");
       }
     });
   });
   /*******************************************************
    投稿画面
    ******************************************************/
-   
-   if (location.href == "https://50bdae72da8d40919c941cb6c4782fc2.vfs.cloud9.ap-northeast-1.amazonaws.com/posts/new"
-       || location.href == "https://50bdae72da8d40919c941cb6c4782fc2.vfs.cloud9.ap-northeast-1.amazonaws.com/posts" ) {
-      // 地図上でクリックしたときのイベント
-      map.addListener('click', function(e) {
-        getClickLatLng(e.latLng, map);
-        getClickAddress(e.latLng);
-        toggleMarker(e.latLng, map);
-      });
-   }
-  
-  
+  if (location.href == "https://50bdae72da8d40919c941cb6c4782fc2.vfs.cloud9.ap-northeast-1.amazonaws.com/posts/new"
+     || location.href == "https://50bdae72da8d40919c941cb6c4782fc2.vfs.cloud9.ap-northeast-1.amazonaws.com/posts" ) {
+    // 地図上でクリックしたときのイベント
+    map.addListener('click', function(e) {
+      console.log("マップクリック")
+      getClickLatLng(e.latLng, map);
+      getClickAddress(e.latLng);
+      toggleMarker(e.latLng, map);
+    });
+  }
+
+
   /********************************************************
    地図一覧画面
    ********************************************************/
-   
+
   if (location.href == "https://50bdae72da8d40919c941cb6c4782fc2.vfs.cloud9.ap-northeast-1.amazonaws.com/map") {
     const response = await fetch("/posts.json").then((res) => res.json()).catch(error => console.error(error));
     const items = response.data.items;
@@ -100,15 +103,15 @@ async function initMap() {
       })
     });
   }
-  
+
   /**********************************************************************
   関数の定義
   ********************************************************************/
   function getClickLatLng(lat_lng, map) {
-    $("#ido").val(lat_lng.lat());
-    $("#keido").val(lat_lng.lng());
+    $("#lat-input").val(lat_lng.lat());
+    $("#lng-input").val(lat_lng.lng());
   }
-  
+
   // リバースジオコーディングで住所を取得
   function getClickAddress(lat_lng) {
     let latlng = {
@@ -139,18 +142,18 @@ async function initMap() {
       } else {
         window.alert("No results found");
       }
-    }).catch((e) => window.alert("Geocoder failed due to: " + e)); 
+    }).catch((e) => window.alert("Geocoder failed due to: " + e));
   }
-  
+
   function toggleMarker(lat_lng, map) {
-    
+
     // もし、直前に追加したマーカーが残っているなら、マップから隠す
     if (marker) {
       marker.setMap(null);
     }
     // マーカーを削除する
     marker = null;
-    
+
     // マーカーを設置
     marker = new google.maps.Marker({
       position: lat_lng,
