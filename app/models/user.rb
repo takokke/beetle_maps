d82eb7510177eaea5d2e6d040d1fc3d3bee0935d
takumi_Mac <>
2024-02-12 14:36:27 +0900

    top修正&ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,17 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+  def guest_user?
+    email == GUEST_USER_EMAIL
+  end
+
 end
